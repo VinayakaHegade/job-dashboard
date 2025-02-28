bcba0e8c48589c515ac83ab46ff5feecfa1a49cb
VinayakaHegade <myselfvinayakahegade@gmail.com>
2025-02-28 10:47:11 +0530

    feat(ui): add horizontal scrollbar for tabs

--- src/components/ui/tabs/TabBar.tsx
+++ src/components/ui/tabs/TabBar.tsx
@@ -8,7 +8,7 @@ type TabBarProps = {
 
 export default function TabBar({ tabs, activeTab, onTabChange }: TabBarProps) {
   return (
-    <section className="flex gap-6 flex-wrap justify-between md:justify-start md:gap-x-[72px]">
+    <section className="flex gap-6 md:gap-x-[72px] justify-between md:justify-start pb-2 whitespace-nowrap overflow-x-auto scroll-smooth sm:pb-0 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thin scrollbar-thumb-zinc-400 scrollbar-track-zinc-100 hover:scrollbar-thumb-zinc-500 ">
       {tabs.map((tab) => (
         <Tab
           key={tab}
